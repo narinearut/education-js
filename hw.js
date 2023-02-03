84e9566e63f5841bc5763d308131bd38a1269b46
Narine <115227487+narinearut@users.noreply.github.com>
2023-02-03 20:42:05 +0000

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,7 +1,29 @@
-const productName = "Droid";
-const pricePerItem = 3500;
+// const productName = "Droid";
+// const pricePerItem = 3500;
 
-// Change code below this line
-const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
 
-console.log(message);
+// console.log(message);
+
+// function checkStorage(available, ordered) {
+//     let message;
+//     // Change code below this line
+//   if (ordered === 0) {
+//     message = "There are no products in the order!";
+//   } else if (ordered > available) {
+//     message = "Your order is too large, there are not enough items in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+//     // Change code above this line
+//     return message;
+//   }
+
+//   checkStorage(100, 130);
+
+
+// const productName = "Repair droid";
+
+// // Якщо у змінній зберігається рядок
+// console.log(productName.length); //fg
