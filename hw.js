bfb0ca7b8d98b34a30171ffd9fd529354be2975e
Narine <115227487+narinearut@users.noreply.github.com>
2023-02-16 13:11:37 +0000

    hw

--- hw.js
+++ hw.js
@@ -290,3 +290,447 @@
 
 
 
+
+// Z 16
+
+// объекты это зарплаты ключ это имя свойство это сумма
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+  
+//   const keys = Object.keys(salaries);
+
+//   for (const key of keys) {
+//     totalSalary += salaries[key];
+//   }
+  
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// console.log('TOTAL SALARY: ', countTotalSalary({ mango: 100, poly: 150, alfred: 80}))
+
+// Z 17
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+//   // Change code below this line
+  
+//   for (const color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb)
+
+//   }
+//   console.log(`hex colors: ${hexColors}, rgb colors: ${rgbColors}`);
+
+
+// Z 18
+
+
+// ПОШУК ОБ'ЄКТА ЗА ЗНАЧЕННЯМ ВЛАСТИВОСТІ
+
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getProductPrice(productName) {
+//     // Change code below this line
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//         return product.price;
+//     }
+//   }
+//   return null;
+//     // Change code above this line
+//   }
+
+// Z 19
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+
+//   function getAllPropValues(propName) {
+
+// const findProduct = [];
+
+//     for (const product of products) {
+//       if (product[propName]) {
+//         findProduct.push(product[propName])
+//       }
+//     }
+// return findProduct;
+//   }
+  
+//   getAllPropValues("name")
+
+//   console.log( getAllPropValues("name"))
+
+// Z 20
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function calculateTotalPrice(productName) {
+//     // Change code below this line
+//   let totalPrice = 0;
+  
+//     for (let product of products) {
+//        if (product.name === productName) {
+//          totalPrice = product.price * product.quantity;
+//          return totalPrice;
+//        }
+//     }
+  
+//   return 0;
+//     // Change code above this line
+//   }
+
+//   calculateTotalPrice("Radar")
+
+//     console.log(calculateTotalPrice("Radar"))
+
+
+// Z 31
+// ...rest 
+
+// Change code below this line
+// function add(...args) {
+//     let total = 0;
+//     for (let arg of args) {
+//       total += arg;
+      
+//     }
+//     // Change code above this line 
+//     return total;
+//   }
+  
+
+
+// Z32
+// Функція addOverNum() рахує суму всіх аргументів. Зміни параметри і тіло функції addOverNum() таким чином, щоб вона рахувала суму тільки тих аргументів, які більші за задане число. Це число повинно бути першим параметром функції.
+
+// function addOverNum(number, ...args) {
+//     let total = 0;
+//     for (const arg of args) {
+//       if (number < arg) {
+//         total += arg;
+//       }
+      
+//     }
+//     return total;
+//   }
+  
+
+// Z 33
+
+// Доповни код функції таким чином, щоб вона повертала новий масив matches, в якому будуть тільки ті аргументи, починаючи з другого, які є в масиві першого аргументу.
+
+// Наприклад, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) повинна повернути масив [1, 2], тому що тільки вони є в масиві першого аргументу.
+
+
+// // Change code below this line
+// function findMatches(numbers, ...args) {
+//     const matches = []; // Don't change this line
+//   for (let number of numbers) {
+//     if (args.includes(number)) {
+//       matches.push(number)
+//     }
+//   }
+//     // Change code above this line
+//     return matches;
+//   }
+
+
+// Z 35
+
+// змінював назву книги з oldName на newName у властивості books. Використовуй indexOf() для того, щоб знайти потрібний елемент масиву, і splice() для того, щоб замінити цей елемент.
+
+// const bookShelf = {
+//     books: ["The last kingdom", "Haze", "The guardian of dreams"],
+//     updateBook(oldName, newName) {
+//       // Change code below this line
+//        const oldNameIndex = this.books.indexOf(oldName);
+//   this.books.splice(oldNameIndex, 1, newName)
+//     },
+
+//   };
+// bookShelf.updateBook("The last kingdom", "Narine");
+// console.log(bookShelf);
+
+
+
+// Z 36-37 Магазин
+// ЗАДАЧА. КРАМНИЦЯ ЗІЛЛЯ «У СТАРОЇ ЖАБИ»
+// ЗАДАЧА. ОТРИМУЄМО ВСЕ ЗІЛЛЯ 
+// return 
+// До нас звернулася власниця крамниці зілля «У старої жаби» і замовила програму для ведення інвентарю - додавання, видалення, пошуку та оновлення зілля. 
+// Додай об'єкту atTheOldToad властивість potions, значенням якої зроби порожній масив.
+
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//     // Change code below this line
+//   getPotions() {
+//   return this.potions;
+//   }
+//     // Change code above this line
+//   };
+  
+
+// Z 38
+// ЗАДАЧА: ДОДАЄМО НОВЕ ЗІЛЛЯ
+// push 
+// Доповни метод addPotion(potionName) таким чином, щоб він додавав зілля potionName в кінець масиву зілля у властивості potions.
+
+// const atTheOldToad = {
+//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//   addPotion(potionName) {
+//     this.potions.push(potionName);
+//   },
+// };
+
+// Z 39
+// ЗАДАЧА: ВИДАЛЯЄМО ЗІЛЛЯ
+// splice 
+// Доповни метод removePotion(potionName) таким чином, щоб він видаляв зілля potionName з масиву зілля у властивості potions.
+
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//     removePotion(potionName) {
+//   const indexPotionName = this.potions.indexOf(potionName);
+//   this.potions.splice(indexPotionName, 1);
+
+//     },
+//   };
+
+// Z 40
+// ЗАДАЧА: ОНОВЛЮЄМО ЗІЛЛЯ 
+// splice
+// Доповни метод updatePotionName(oldName, newName) таким чином, щоб він оновлював назву зілля з oldName на newName в масиві зілля у властивості potions.
+
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//     updatePotionName(oldName, newName) {
+
+//       const indexOldName = this.potions.indexOf(oldName);
+//       this.potions.splice(indexOldName, 1, newName)
+
+//     },
+//   };
+
+// Z 41
+// const atTheOldToad = {
+//     potions: [
+//       { name: "Speed potion", price: 460 },
+//       { name: "Dragon breath", price: 780 },
+//       { name: "Stone skin", price: 520 },
+//     ],
+//     // Change code below this line
+//     getPotions() {
+//       return this.potions;
+//     },
+//     addPotion(newPotion) {
+//       for (let potion of this.potions) {
+//           if (potion.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       } }
+// this.potions.push(newPotion);
+      
+//     },
+
+//     removePotion(potionName) {
+
+//         for (let i = 0; i < this.potions.length; i+=1) {
+// const potion = this.potions[i];
+
+//           if ( potion.name === potionName) {
+//  return this.potions.splice(i, 1);
+//         } }
+//           return `Potion ${potionName} is not in inventory!`;
+//       },
+  
+//       updatePotionName(oldName, newName) {
+//         // const potionIndex = this.potions.indexOf(oldName);
+    
+//         for (let i = 0; i < this.potions.length; i+=1) {
+
+            
+//                       if ( this.potions[i].name === oldName) {
+//              return this.potions[i].name = newName;
+//                     } }
+//  return `Potion ${oldName} is not in inventory!`;
+//       },
+// }
+
+// debugger;
+
+    // console.log(atTheOldToad.addPotion({ name: 'Invisibility', price: 620 }));
+
+    // // console.log(atTheOldToad.removePotion("Dragon breath"));
+
+    // console.log(atTheOldToad.potions);
+
+    // console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"));
+
+    // console.log(atTheOldToad.potions);
+// module 4
+// Z 2
+// function deliverPizza(pizzaName) {
+//     return `Delivering ${pizzaName} pizza.`;
+//   }
+  
+//   function makePizza(pizzaName) {
+//     return `Pizza ${pizzaName} is being prepared, please wait...`;
+    
+//   }
+  
+//   // Chande code below this line
+//   function makeMessage(pizzaName, callback) {
+//     return callback(pizzaName);
+//   }
+  
+//   console.log(makeMessage('pepperoni', makePizza));
+
+
+// Z 3
+// function makePizza(pizzaName, callback) {
+//     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//     callback(pizzaName);
+//   }
+  
+//   makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//     console.log(`Delivering pizza ${pizzaName}.`);
+//   });
+//   // Change code below this line
+  
+//   makePizza("Ultracheese", function eatPizza(pizzaName) {
+//     console.log(`Eating pizza ${pizzaName}`)
+//   });
+  
+
+// z 4
+
+// const pizzaPalace = {
+//     pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//     order(pizzaName, onSuccess, onError) {
+//       if (!this.pizzas.includes(pizzaName)) {
+//         return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//       }
+//       return onSuccess(pizzaName);
+//     },
+//   };
+//   // Change code above this line
+  
+//   // Callback for onSuccess
+//   function makePizza(pizzaName) {
+//     return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+//   }
+  
+//   // Callback for onError
+//   function onOrderError(error) {
+//     return `Error! ${error}`;
+//   }
+  
+//   // Method calls with callbacks
+//   console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+//   console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+//   console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+//   console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+
+
+// Z 5
+// forEach масив чисел, і розраховує загальну суму його елементів
+
+
+// function calculateTotalPrice(orderedItems) {
+//     let totalPrice = 0;
+//     // Change code below this line
+  
+//      orderedItems.forEach(function (number, index) {
+//       totalPrice += number;
+//     });
+  
+//     // Change code above this line
+//     return totalPrice;
+//   }
+//   console.log(calculateTotalPrice([164, 48, 291]));  
+
+// Z 6
+// forEach перебирання МАСИВУ ЧИСЕЛ
+// Функція filterArray(numbers, value) приймає масив чисел numbers і повертає новий масив, в якому будуть тільки ті елементи оригінального масиву, які більші за значення параметра value
+
+// function filterArray(numbers, value) {
+//     const filteredNumbers = [];
+//     // Change code below this line
+  
+//     numbers.forEach(function (number, index) {
+//       if (number > value) {
+//         filteredNumbers.push(number);
+//       }
+//     }
+//     )
+  
+//     // Change code above this line
+//     return filteredNumbers;
+//   }
+  
+//   console.log(filterArray([1, 2, 3, 4, 5], 3))
+
+// Z 7
+// СПІЛЬНІ ЕЛЕМЕНТИ через forEach
+
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+//     // Change code below this line
+  
+//     firstArray.forEach(function(firstElement, index) {
+//       if (secondArray.includes(firstElement)) {
+//         commonElements.push(firstElement);
+//       }
+//     }
+//     )
+//     return commonElements;
+//     // Change code above this line
+//   }
+  
+//   console.log(getCommonElements([1, 2, 3], [2, 4]))
+
+// z11
+// ФІЛЬТРАЦІЯ МАСИВУ ЧИСЕЛ 2.0 cтрілочна ф-ція
+// Change code below this line
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//     const filteredNumbers = [];
+  
+//     numbers.forEach (number => {
+//       if (number > value) {
+//         filteredNumbers.push(number);
+//       }
+//     });
+  
+//     // Change code above this line
+//     return filteredNumbers;
+//   }
+  
+//   console.log(filterArray([1, 2, 3, 4, 5], 3))
